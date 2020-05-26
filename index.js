72d6dfcffd419b0503001e9d14d85c68d26cdce3
Takahiro Ueda <tkhr6022@gmail.com>
2020-05-27 05:02:00 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+(function () {
+  const wheel = document.querySelector(".wheel");
+  const startButton = document.querySelector(".button");
+  let deg = 0;
+
+  startButton.addEventListener("click", () => {
+    startButton.style.pointerEvents = "none";
+    startButton.style.position ='relative'; 
+    startButton.style.top =1 +'px'; 
+    startButton.style.left = 1 +'px'; 
+    startButton.style.opacity = 0.5; 
+
+    deg = Math.floor(5000 + Math.random() * 5000);
+
+    wheel.style.transition = "all 10s ease-out";
+
+
+    wheel.style.transform = `rotate(${deg}deg)`;
+    
+  });
+
+  wheel.addEventListener("transitionend", () => {
+    
+    startButton.style.pointerEvents = "auto";
+
+
+    startButton.style.position ='relative'; 
+    startButton.style.top =0 +'px'; 
+    startButton.style.left =0 +'px'; 
+    startButton.style.opacity = 1;
+
+
+    wheel.style.transition = "none";
+
+    const actualDeg = deg % 360;
+
+    wheel.style.transform = `rotate(${actualDeg}deg)`;
+  });
+})();
